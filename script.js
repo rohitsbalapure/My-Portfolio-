100f1590a02a50831f1fd4a1a95fd19c797fb084
Rohit Balapure <rohitbalapure@gmail.com>
2024-02-21 16:05:27 +0530

    Update script.js
    New functions added

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   //sticky header
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 1) {
-        $(".header-area").addClass("sticky");
-      } else {
-        $(".header-area").removeClass("sticky");
-      }
-  
-      // Update the active section in the header
-      updateActiveSection();
-    });
-  
-    $(".header ul li a").click(function(e) {
-      e.preventDefault(); 
-  
-      var target = $(this).attr("href");
-  
-      if ($(target).hasClass("active-section")) {
-        return; 
-      }
-  
-      if (target === "#home") {
-        $("html, body").animate(
-          {
-            scrollTop: 0 
-          },
-          500
-        );
-      } else {
-        var offset = $(target).offset().top - 40; 
-  
-        $("html, body").animate(
-          {
-            scrollTop: offset
-          },
-          500
-        );
-      }
-  
-      $(".header ul li a").removeClass("active");
-      $(this).addClass("active");
-    });
-  
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      $(".header-area").addClass("sticky");
+    } else {
+      $(".header-area").removeClass("sticky");
+    }
 
-    //Initial content revealing js
-    ScrollReveal({
-      distance: "100px",
-      duration: 2000,
-      delay: 200
-    });
-  
-    ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", {
-      origin: "left"
-    });
-    ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", {
-      origin: "right"
-    });
-    ScrollReveal().reveal(".project-title, .contact-title", {
-      origin: "top"
-    });
-    ScrollReveal().reveal(".projects, .contact", {
-      origin: "bottom"
-    });
+    // Update the active section in the header
+    updateActiveSection();
+  });
+
+  $(".header ul li a").click(function (e) {
+    e.preventDefault();
+
+    var target = $(this).attr("href");
+
+    if ($(target).hasClass("active-section")) {
+      return;
+    }
+
+    if (target === "#home") {
+      $("html, body").animate(
+        {
+          scrollTop: 0
+        },
+        500
+      );
+    } else {
+      var offset = $(target).offset().top - 40;
+
+      $("html, body").animate(
+        {
+          scrollTop: offset
+        },
+        500
+      );
+    }
+
+    $(".header ul li a").removeClass("active");
+    $(this).addClass("active");
+  });
+
+
+  //Initial content revealing js
+  ScrollReveal({
+    distance: "100px",
+    duration: 2000,
+    delay: 200
+  });
+
+  ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", {
+    origin: "left"
+  });
+  ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", {
+    origin: "right"
+  });
+  ScrollReveal().reveal(".project-title, .contact-title", {
+    origin: "top"
+  });
+  ScrollReveal().reveal(".projects, .contact", {
+    origin: "bottom"
+  });
 
   //contact form to excel sheet
   const scriptURL = 'https://docs.google.com/spreadsheets/d/1Fgwz754UzpY5K63T_uZ3PvLHSel-LtGpo9IIKlYp2YQ/edit?usp=sharing';
@@ -70,45 +70,63 @@ $(document).ready(function() {
   const msg = document.getElementById("msg")
 
   form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-          .then(response => {
-              msg.innerHTML = "Message sent successfully"
-              setTimeout(function () {
-                  msg.innerHTML = ""
-              }, 5000)
-              form.reset()
-          })
-          .catch(error => console.error('Error!', error.message))
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function () {
+          msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
   })
-    
-  });
-  
-  function updateActiveSection() {
-    var scrollPosition = $(window).scrollTop();
-  
-    // Checking if scroll position is at the top of the page
-    if (scrollPosition === 0) {
-      $(".header ul li a").removeClass("active");
-      $(".header ul li a[href='#home']").addClass("active");
-      return;
-    }
-  
-    // Iterate through each section and update the active class in the header
-    $("section").each(function() {
-      var target = $(this).attr("id");
-      var offset = $(this).offset().top;
-      var height = $(this).outerHeight();
-  
-      if (
-        scrollPosition >= offset - 40 &&
-        scrollPosition < offset + height - 40
-      ) {
-        $(".header ul li a").removeClass("active");
-        $(".header ul li a[href='#" + target + "']").addClass("active");
-      }
-    });
-  }
-  
 
- 
+});
+
+function updateActiveSection() {
+  var scrollPosition = $(window).scrollTop();
+
+  // Checking if scroll position is at the top of the page
+  if (scrollPosition === 0) {
+    $(".header ul li a").removeClass("active");
+    $(".header ul li a[href='#home']").addClass("active");
+    return;
+  }
+
+  // Iterate through each section and update the active class in the header
+  $("section").each(function () {
+    var target = $(this).attr("id");
+    var offset = $(this).offset().top;
+    var height = $(this).outerHeight();
+
+    if (
+      scrollPosition >= offset - 40 &&
+      scrollPosition < offset + height - 40
+    ) {
+      $(".header ul li a").removeClass("active");
+      $(".header ul li a[href='#" + target + "']").addClass("active");
+    }
+  });
+}
+
+AOS.init();
+// ------------Paragraph Printing
+
+var paragraph = document.getElementById('displayParagraph');
+    
+var words = paragraph.innerText.split(' ');
+
+paragraph.innerHTML = '';
+
+var i = 0;
+var intervalId = setInterval(function () {
+    paragraph.innerHTML += words[i] + ' ';
+    i++;
+
+    if (i === words.length) {
+        clearInterval(intervalId);
+    }
+}, 200);
+
+
